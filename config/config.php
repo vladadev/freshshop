ac5b1587f561d45014883b5239609c3c963d76ff
vladadev <89929541+vladadev@users.noreply.github.com>
2021-09-26 05:58:57 +0200

    Config update
    Config update

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 
 //Osnovna podesavanja
-define("BASE_URL", "http://freshshop.great-site.net/");
-define("ABSOLUTE_PATH", $_SERVER["DOCUMENT_ROOT"]."/freshshop06-03/");
+define('BASE_URL', '');
+define('NEW_BASE_URL', 'http://localhost/freshshop/');
+define('ABSOLUTE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/freshshop/');
 
 //Podesavanja za bazu
-define("SERVER", "sql304.epizy.com");
-define("DATABASE", "epiz_28100944_php1project");
-define("USERNAME", "epiz_28100944");
-define("PASSWORD", "d0U1sBVh0ho3ZLa");
+define('SERVER', 'localhost');
+define('DATABASE', 'freshshop');
+define('USERNAME', 'root');
+define('PASSWORD', '');
